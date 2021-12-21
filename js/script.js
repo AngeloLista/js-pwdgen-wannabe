1ee2fc46a407ba3566193bd9a6f749a1a4897b82
AngeloLista <abstractplayer@gmail.com>
2021-12-21 15:29:19 +0100

    added basic program

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log('JS OK')
+/*  - Chiedi all’utente il suo nome,
+    - poi chiedi il suo cognome,
+    - poi chiedi il suo colore preferito
+    - Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+    Bonus:
+    - scrivere "La tua password è nomecogonomecolorepreferito21"
+    - Abbellire la pagina dal punto di vista grafico a piacimento! */
+
+console.log('JS OK');
+
+const firstName = prompt('Qual è il tuo nome?')
+const lastName = prompt('Qual è il tuo cognome')
+const favouriteColor = prompt('Qual è il tuo colore preferito?')
+
+document.writeln(firstName + lastName + '21')
